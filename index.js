9dddf9a0e29d11f671014c5e95ad0669ba989e0c
pavpench <p.penchev90@gmail.com>
2024-02-27 08:35:21 +0100

    data-set added to nav buttons

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
 "use strict";
 
 const navContainer = document.querySelector(".nav-container");
+const nav_btns = document.querySelectorAll(".nav_btn");
+const nav_content = document.querySelectorAll(".nav_content");
 const overlay = document.querySelector(".overlay");
 
 const info = document.querySelector(".info");
 
-// contact.addEventListener("click", function () {
-//   modal.style.display = "block";
-// });
-
 //Event handling delegation
 navContainer.addEventListener("click", function (e) {
-  const clicked = e.target.closest(".nav-btn");
+  const clicked = e.target.closest(".nav_btn");
 
   if (!clicked) return;
 
   if (clicked === info) {
     overlay.classList.remove("hidden");
   } else overlay.classList.add("hidden");
+
+  nav_btns.forEach((navTab) => navTab.classList.remove("nav_btn--active"));
+  nav_content.forEach((content) =>
+    content.classList.remove("nav_content--active")
+  );
+
+  clicked.classList.add("nav_btn--active");
+
+  document
+    .querySelector(`.nav_content--${clicked.dataset.tab}`)
+    .classList.add("nav_content--active");
+
   console.log(clicked);
 });
