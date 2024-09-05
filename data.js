9174ea008cfa2fce083ce8ccb692a3e19e8b6fd0
pavpench <p.penchev90@gmail.com>
2024-09-05 13:05:16 +0200

    rework of gallery filters

---
+++ data.js
@@ -0,0 +1,7 @@
+const categories = [
+  { name: "All", filter: "all" },
+  { name: "Newborn", filter: "newborn" },
+  { name: "Baptism", filter: "baptism" },
+  { name: "Pregnant", filter: "pregnant" },
+  { name: "Ocassions", filter: "occasions" },
+];
